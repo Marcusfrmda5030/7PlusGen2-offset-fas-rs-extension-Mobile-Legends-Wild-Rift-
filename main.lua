7173fd0fd156c8a3b7226179ec92f43fac47ded2
Marcusfrmda5030 <105986930+Marcusfrmda5030@users.noreply.github.com>
2025-04-18 19:23:46 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,61 +1,16 @@
 API_VERSION = 4
 
 function load_fas(pid, pkg)
-    if (pkg == "com.tencent.tmgp.sgame")
-    then
-        set_extra_policy_rel(0, 4, -300000, -100000)
-        set_extra_policy_abs(7, 2000000, 2750000)
-    elseif (pkg == "com.tencent.tmgp.pubgmhd" or pkg == "com.rekoo.pubgm" or pkg == "com.tencent.ig" or pkg == "com.vng.pubgmobile" )
-    then
-        set_extra_policy_rel(4, 7, -250000, -150000)
-    elseif (pkg == "com.miHoYo.Yuanshen" or pkg == "com.miHoyo.ys.bilibili" or pkg == "com.miHoYo.GenshinImpact" )
-    then
-        set_extra_policy_rel(0, 4, -150000, 0)
-    elseif (pkg == "com.miHoYo.Nap" or pkg == "com.miHoYo.Nap.bilibili")
-    then
-	    set_ignore_policy(0, true)
-        set_extra_policy_abs(4, 2000000, 2350000)
-        set_extra_policy_rel(4, 7, -150000, 0)
-    elseif (pkg == "com.miHoYo.hkrpg" or pkg == "com.miHoYo.hkrpg.bilibili")
-    then
-	    set_ignore_policy(0, true)
-        set_extra_policy_abs(4, 2000000, 2350000)
-        set_extra_policy_rel(4, 7, -150000, 0)
-    elseif (pkg == "com.netease.aceracer")
-    then
-        set_extra_policy_rel(4, 7, -300000, -100000)
-    elseif (pkg == "com.tencent.KiHan")
-    then
-        set_extra_policy_rel(0, 4, -300000, -100000)
-    elseif (pkg == "com.tencent.tmgp.cod")
-    then
-        set_extra_policy_rel(0, 4, -250000, -100000)
-    elseif (pkg == "com.netease.l22")
-    then
-        set_extra_policy_rel(0, 4, -350000, -100000)
-    elseif (pkg == "com.tencent.tmgp.cf")
-    then
-        set_extra_policy_rel(4, 7, -250000, -150000)
-    elseif (pkg == "com.netease.dfjs" or pkg == "com.netease.dfjs.mi")
-    then
-        set_extra_policy_rel(0, 4, -300000,-100000)
-    elseif (pkg == "com.tencent.lolm")
-    then
-        set_extra_policy_rel(0, 4, -300000, -100000)
-    elseif (pkg == "com.kurogame.mingchao")
-    then
-        set_extra_policy_rel(0, 4, -300000, -100000)
-        set_extra_policy_abs(7, 2300000, 2800000)
-    elseif (pkg == "com.tencent.mf.uam" or pkg == "com.proximabeta.mf.uamo" )
-    then
-        set_extra_policy_rel(4, 7, -300000, -150000)
-    elseif (pkg == "com.tencent.tmgp.dfm")
-    then
-        set_extra_policy_rel(4, 7, -300000, -150000)
-    elseif (pkg == "com.netease.dwrg.nearme.gamecenter")
-    then
-        set_extra_policy_rel(0, 4, -300000, -150000)
-	set_extra_policy_rel(4, 7, -250000, -150000)
+    if (pkg == "com.mobile.legends") then
+        set_extra_policy_rel(0, 4, -75000, 0)
+        set_extra_policy_abs(0, 1400000, 1700000)
+        set_extra_policy_rel(4, 7, -50000, 0)
+        set_extra_policy_abs(4, 1800000, 2200000)
+        set_ignore_policy(7, true)
+
+    elseif (pkg == "com.tencent.lolm") then
+        set_extra_policy_rel(0, 4, -100000, 0)
+        set_extra_policy_abs(4, 1800000, 2400000)
     end
 end
 
